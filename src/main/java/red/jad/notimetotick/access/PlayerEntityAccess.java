731cec35a53b0029c2f27fa03b5644085eeb53bd
jadc <chehimj@gmail.com>
2020-07-09 19:43:51 -0600

    wtf is going on

--- src/main/java/red/jad/notimetotick/access/PlayerEntityAccess.java
+++
@@ -1,6 +0,0 @@
-package red.jad.notimetotick.access;
-
-public interface PlayerEntityAccess {
-    int getStoredTicks();
-    void setStoredTicks(int ticks);
-}
--- src/main/java/red/jad/notimetotick/backend/Config.java
+++
@@ -1,7 +0,0 @@
-package red.jad.notimetotick.backend;
-
-public class Config {
-    public static final int ticksPerSecond = 20;
-    public static final int tickingDuration = 30;
-    public static final int cooldownDuration = 10;
-}
